48bcbbd4393cc98d01c92c5aaf344f559e387caa
Gusrot <gnrotela@yahoo.com.ar>
2016-09-06 12:25:04 -0300

    Add files via upload

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aula miPrimerAula=new Aula();
+            Aula.mostrarAlumno(miPrimerAula);
+            bool existe;
+
+            Alumno alumno1 = new Alumno("Pollo", 1);
+            Alumno alumno2 = new Alumno("Rotela", 2);
+            Alumno alumno3 = new Alumno("Ortiz", 3);
+            Alumno alumno4 = new Alumno("Otero", 4);
+
+            /*miPrimerAula.agregarAlumno(alumno1);
+            miPrimerAula.agregarAlumno(alumno2);
+            miPrimerAula.agregarAlumno(alumno3);
+            miPrimerAula.borrarAlumno(alumno2);
+            miPrimerAula.agregarAlumno(alumno4);
+            miPrimerAula.agregarAlumno(alumno1);
+            miPrimerAula.agregarAlumno(alumno2);
+            miPrimerAula.agregarAlumno(alumno3);*/
+            miPrimerAula = miPrimerAula + alumno1;
+            //miPrimerAula.listaDeAlumno[0].nombre="lalala";
+            existe=miPrimerAula.existeAlumno(alumno2);
+            Aula.mostrarAlumno(miPrimerAula);
+            Console.WriteLine(existe);
+            Console.ReadKey();
+        }
+
+    }
+}
